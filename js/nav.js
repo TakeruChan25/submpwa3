69f3b4caa49a9dbfa67b480e69ab4652efded83d
TakeruChan25 <ajimut2505@gmail.com>
2020-11-09 20:21:21 +0700

    submpwa2

--- js/nav.js
+++ js/nav.js
@@ -35,10 +35,10 @@ document.addEventListener("DOMContentLoaded", function() {
         xhttp.send();
     }
 
-    // // Load page content
-    // var page = window.location.hash.substr(1);
-    // if (page == "") page = "home";
-    // loadPage(page);
+    // Load page content
+    var page = window.location.hash.substr(1);
+    if (page == "") page = "home";
+    loadPage(page);
     
     // function loadHomeCourse() {
     //   document.querySelectorAll(".card-action a").forEach(function(elm) {
@@ -56,26 +56,26 @@ document.addEventListener("DOMContentLoaded", function() {
     // }
 
 
-    // function loadPage(page) {
-    //   var xhttp = new XMLHttpRequest();
-    //   xhttp.onreadystatechange = function() {
-    //       if (this.readyState == 4) {
-    //         var content = document.querySelector(".body-content");
-    //         if (this.status == 200) {
-    //             content.innerHTML = xhttp.responseText;
+    function loadPage(page) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            var content = document.querySelector(".body-content");
+            if (this.status == 200) {
+                content.innerHTML = xhttp.responseText;
                 
-    //         } else if (this.status == 404) {
-    //             content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-    //         } else {
-    //             content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
-    //         }
-    //         var cardaction = document.querySelectorAll("#body-content .card-action a")
-    //         if(cardaction.length > 0) {
-    //           loadHomeCourse();
-    //         }
-    //       }
-    //   };
-    //   xhttp.open("GET", "/pages/" + page + ".html", true);
-    //   xhttp.send();
-    // }
+            } else if (this.status == 404) {
+                content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+            } else {
+                content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+            }
+            var cardaction = document.querySelectorAll("#body-content .card-action a")
+            if(cardaction.length > 0) {
+              loadHomeCourse();
+            }
+          }
+      };
+      xhttp.open("GET", "/pages/" + page + ".html", true);
+      xhttp.send();
+    }
   });
