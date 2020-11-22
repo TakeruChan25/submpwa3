e9752f7205eeacba0134869c5ef4607bfe173c4d
TakeruChan25 <ajimut2505@gmail.com>
2020-11-22 15:27:04 +0700

    submpwa3

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 // if (workbox) console.log("workbox ok");
 if (workbox)
